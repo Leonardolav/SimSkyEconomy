27766544afdb463c3ea6318b48ad426de1b05129
Leonardolav <leonardo.lavourinha@gmail.com>
2025-04-15 16:12:44 -0300

    at

--- simskyeconomy/simskyeconomy/settings.py
+++ simskyeconomy/simskyeconomy/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_htmx.middleware.HtmxMiddleware',
+    'ratelimit.middleware.RatelimitMiddleware',
     
 ]
 
